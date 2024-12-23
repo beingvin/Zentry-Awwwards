f5a0a773a50de14f613a274c70a3b882a33960c9
beingvin <beingvini@gmail.com>
2024-12-23 17:15:08 +0530

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sanf-serif"],
+        genaral: ["general", "sanf-serif"],
+        "circular-web": ["circular-web", "sanf-serif"],
+        "robert-medium": ["robert-meduum", "sanf-serif"],
+        "robert-regular": ["robert-regular", "sanf-serif"],
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "DFDFF2",
+          100: "F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724FF",
+        },
+        yellow: {
+          100: "#8E983F",
+          300: "#EDFF66",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
